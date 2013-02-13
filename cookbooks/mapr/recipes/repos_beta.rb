c5b92b7d96b305b7be341155f7aeb9800909a788
Brad Anderson <brad@sankatygroup.com>
2013-02-13 12:15:01 -0500

    public beta repo

--- cookbooks/mapr/recipes/repos_beta.rb
+++ cookbooks/mapr/recipes/repos_beta.rb
@@ -5,10 +5,16 @@
 # Copyright 2013, MapR Technologies
 #
 
+# TODO: DRY
+
 if platform?("redhat", "centos")
 
   yum_repository "mapr_core" do
-    uri "http://yum.qa.lab/mapr-beta"
+    creds = ""
+    if node[:mapr][:beta_u]
+      creds = "#{node[:mapr][:beta_u]}:#{node[:mapr][:beta_p]}@"
+    end
+    uri "http://#{creds}stage.mapr.com/beta/v3.0-beta/redhat"
   end
 
 end
@@ -16,7 +22,11 @@ end
 if platform?("ubuntu")
 
   apt_repository "mapr_core" do
-    uri "http://apt.qa.lab/mapr-beta"
+    creds = ""
+    if node[:mapr][:beta_u]
+      creds = "#{node[:mapr][:beta_u]}:#{node[:mapr][:beta_p]}@"
+    end
+    uri "http://#{creds}stage.mapr.com/beta/v3.0-beta/ubuntu"
     notifies :run, resources(:execute => "apt-get update")
   end
 
