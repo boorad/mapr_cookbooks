552a07809c9fdb497c212d49354ed5164f13742f
Brad Anderson <brad@sankatygroup.com>
2013-05-07 08:08:51 -0400

    remove beta roles, version bump

--- chef/cookbooks/mapr/attributes/default.rb
+++ chef/cookbooks/mapr/attributes/default.rb
@@ -12,7 +12,7 @@ default[:mapr][:ip] = "1.1.1.1"
 
 default[:mapr][:home] = "/opt/mapr"
 default[:mapr][:clustername] = "my.cluster.com"
-default[:mapr][:version] = "2.1.1"
+default[:mapr][:version] = "2.1.2"
 default[:mapr][:repo_url] = "http://package.mapr.com/releases"
 
 default[:mapr][:disks] = ["/dev/sdb","/dev/sdc","/dev/sdd"]
