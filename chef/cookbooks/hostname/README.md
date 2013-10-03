13bbc53532335298915686f9d469c632899a9c09
Brad Anderson <brad@sankatygroup.com>
2013-10-03 10:15:23 -0400

    zap most cookbooks in favor of berkshelf mgmt

--- chef/cookbooks/hostname/README.md
+++
@@ -1,9 +0,0 @@
-# DESCRIPTION:
-
-Sets hostname and FQDN of the node.
-
-# ATTRIBUTES:
-
-set_fqdn - FQDN to set
-
-The latest code is hosted at https://github.com/3ofcoins/chef-cookbook-hostname
--- chef/cookbooks/hostname/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Maciej Pasternacki"
-maintainer_email "maciej@pasternacki.net"
-license          "MIT"
-description      "Configures hostname and FQDN"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
-
-supports "debian"
-supports "ubuntu"
