34a74644c64c827a933157c9a4b13c4935435717
Brad Anderson <brad@sankatygroup.com>
2013-12-26 11:02:22 -0500

    nfs needs start on boot & restart of rpcbind

--- chef/cookbooks/mapr/recipes/nfs.rb
+++ chef/cookbooks/mapr/recipes/nfs.rb
@@ -5,8 +5,11 @@
 # Copyright 2013, MapR Technologies
 #
 
+# MapR package
 package "mapr-nfs"
 
+
+# supporting packages
 if platform_family?("rhel")
   package "nfs-utils"
 end
@@ -17,4 +20,13 @@ end
 
 if platform_family?("suse")
   package "nfs-client"
+end
+
+
+# rpcbind
+package "rpcbind"
+
+service "rpcbind" do
+  action :enable
+  action :restart
 end
